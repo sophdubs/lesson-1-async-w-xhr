a095f4bce4a92a11b8a4d9676ca475e06519bf65
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-01-10 13:52:40 +1100

    Added the articleRequest and added onload and onerror for both XMLHttpRequests

--- app.js
+++ app.js
@@ -7,24 +7,61 @@
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
-        // searchedForText = searchField.value;
+        searchedForText = searchField.value;
 
 
-        const searchedForText = 'hippos';
-        const unsplashRequest = new XMLHttpRequest();
+        const imgRequest = new XMLHttpRequest();
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function(err){
+            requestError(err, 'image');
+        };
+        imgRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imgRequest.setRequestHeader('Authorization', 'Client-ID e4b0472678dc5bdb0000500728262e20d2a04a640f470cc6381e81f07d25bac4');
+        imgRequest.send();
 
-        unsplashRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        unsplashRequest.onload = addImage;
-        unsplashRequest.setRequestHeader('Authorization', 'Client-ID e4b0472678dc5bdb0000500728262e20d2a04a640f470cc6381e81f07d25bac4');
 
-        unsplashRequest.send();
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function(err){
+            requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5e38ae2d684c476e9c5ccb956128e301`);
+        articleRequest.send();
+
 
         function addImage(){
-            debugger;
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data && data.results && data.results[0]) {
+                const firstImage = data.results[0];
+                htmlContent = `<figure>
+                    <img src = "${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
+            } else {
+                htmlContent = '<div class="error-no-image">No images available</div>'
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
         }
 
 
+        function addArticles () {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if(data.response && data.response.docs && data.response.docs.length > 1){
+                htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                        <p>${article.snippet}</p>
+                    </li>`
+                ).join('')+'</ul>';
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
     });
-
-
 })();
