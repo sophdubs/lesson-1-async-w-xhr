84fcdf26c2891660997b10672d539ffb1ad1c290
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-01-10 13:18:38 +1100

    Started simple XMLHttpRequest. No response yet

--- app.js
+++ app.js
@@ -7,6 +7,24 @@
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
+        // searchedForText = searchField.value;
+
+
+        const searchedForText = 'hippos';
+        const unsplashRequest = new XMLHttpRequest();
+
+        unsplashRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID e4b0472678dc5bdb0000500728262e20d2a04a640f470cc6381e81f07d25bac4');
+
+        unsplashRequest.send();
+
+        function addImage(){
+            debugger;
+        }
+
+
     });
+
+
 })();
